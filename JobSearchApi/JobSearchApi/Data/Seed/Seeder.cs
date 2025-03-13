c32c395a0b8266adccdc738858d445c5015499b2
thatshowmafiaworks <shinehd56@gmail.com>
2025-03-14 01:58:33 +0200

    added DbSeeder

---
+++ JobSearchApi/JobSearchApi/Data/Seed/Seeder.cs
@@ -0,0 +1,36 @@
+﻿using JobSearchApi.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace JobSearchApi.Data.Seed
+{
+    public class Seeder
+    {
+        public async Task Seed(ILogger<Seeder> logger,
+        RoleManager<IdentityRole> roleManager,
+        UserManager<ApplicationUser> userManager)
+        {
+            if (roleManager.Roles.Count() == 0)
+            {
+                logger.LogCritical("There is no roles");
+                var adminRole = new IdentityRole("Admin");
+                var userRole = new IdentityRole("User");
+                await roleManager.CreateAsync(adminRole);
+                await roleManager.CreateAsync(userRole);
+            }
+
+            var admin = await userManager.FindByNameAsync("Admin");
+            if (admin is null)
+            {
+                var newAdmin = new ApplicationUser
+                {
+                    UserName = "Admin",
+                    Email = "Admin@admin.com"
+                };
+                await userManager.CreateAsync(newAdmin, "Admin_123");
+                await userManager.AddToRoleAsync(newAdmin, "Admin");
+            }
+            roleManager.Roles.ToList().ForEach(x => logger.LogCritical($"Role ID: {x.Id}\t {x.Name}\n"));
+
+        }
+    }
+}
